93e3f6766c4dcdd5d27acc5831bfd2f67e25cf80
root <root@localhost.localdomain>
2019-02-14 12:13:51 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# [News Aggregator](https://github.com/mrlibertarian/NewsAggregator/)
+
+[![License](https://img.shields.io/badge/license-GPL-yellow.svg)][license]
+
+[license]: https://www.gnu.org/licenses/gpl.html
+
+
+## Simple news aggregator written in Go
+
+## Getting started
+
+- Add your favorite RSS feeds to `news.txt`
+- Run aggregator: `go run main.go news.txt`
+
+#### Default port is 800. It can be changed in main.go line 75 (	`var Port string = "800"` ).
+
+## Donation
+
+[![Bitcoin QR-code](blob/master/Images/Bitcoin-QR.png)](bitcoin:15EmrTsRjFuuiRgohSPKqDjjAXdisWULbs)
+
+**BTC 15EmrTsRjFuuiRgohSPKqDjjAXdisWULbs**
---
+++ main.go
@@ -0,0 +1,86 @@
+// News agregator
+
+package main
+
+import ("fmt"
+        "net/http"
+        "io/ioutil"
+        "os"
+        "bufio"
+        "encoding/xml"
+		    "html/template"
+		    "regexp"
+		    "strings")
+
+type News struct {
+		Titles []string `xml:"channel>item>title"`
+		Locations []string `xml:"channel>item>link"`
+		Descriptions []string `xml:"channel>item>description"`
+}
+
+type NewsMap struct {
+	Location string
+	Description string
+	Source string
+}
+
+type NewsAgg struct {
+	Title string
+	News map[string]NewsMap
+}
+
+func index_handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, `<html>
+    <head><title>Index</title>
+    <body>
+    <center>
+    <p><a href="/news/">News Aggregator</a></p>
+    <p><a href="/about/">About</a></p>
+    </center>
+    </body>
+    </html>`)
+}
+
+func newsAgg_handler(w http.ResponseWriter, r *http.Request) {
+	var n News
+
+	regexpSource, _ := regexp.Compile("(www.|)([a-zA-Z0-9+]+)\\.([a-zA-Z]+)")
+	regexpDescription1, _ := regexp.Compile("<br.*>")
+	regexpDescription2, _ := regexp.Compile("<img.*>")
+
+	news_map := make(map[string]NewsMap)
+	file_location := os.Args[1]	// Text file location (First argument)
+	file, _ := os.Open(file_location)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+					resp, _ := http.Get(scanner.Text())
+					bytes, _ := ioutil.ReadAll(resp.Body)
+					xml.Unmarshal(bytes, &n)
+					for idx, _ := range n.Locations {
+						news_map[n.Titles[idx]] = NewsMap{n.Locations[idx], regexpDescription2.ReplaceAllString(regexpDescription1.ReplaceAllString(n.Descriptions[idx], ""), ""), strings.ToUpper(regexpSource.FindString(n.Locations[idx]))}
+					}
+	}
+
+	p := NewsAgg{Title: "Recent News", News: news_map }
+	t, _ := template.ParseFiles("newstemplate.html")
+	t.Execute(w,p)
+}
+
+func about_handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About page!!!")
+}
+
+func main() {
+	var Port string = "800"
+
+	var ServePort string = ":" + Port
+
+	fmt.Printf("Serving on http://127.0.0.1%s", ServePort)
+	http.HandleFunc("/", index_handler)
+	http.HandleFunc("/about", about_handler)
+	http.HandleFunc("/about/", about_handler)
+	http.HandleFunc("/news", newsAgg_handler)
+	http.HandleFunc("/news/", newsAgg_handler)
+	http.ListenAndServe(ServePort, nil)
+}
